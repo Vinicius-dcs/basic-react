5bdd13326315e7e5043f79350af154550448ee07
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-09 16:52:42 -0300

    M3 L2: Style Inline Dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 function App() {
+
+	const [padding, setPadding] = useState(0);
+
+	const handleClick = () => {
+		setPadding(20)
+	}
 
 	return (
 		<div>
-			<button style={{backgroundColor: '#FF0000', color: '#FFF'}}>Clique aqui</button>
+			<button style={{backgroundColor: '#FF0000', color: '#FFF', padding}} onClick={handleClick}>Clique aqui</button>
 		</div>
 	);
-	
+
 }
 
 export default App;
