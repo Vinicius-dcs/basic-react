ef74f45fb5db67b1c688cfbce10ec19bcb37a848
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-09 15:48:47 -0300

    LESSON: Create Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+
 function App() {
 
+	const [name, setName] = useState('...');
+	const [lastName, setLastName] = useState('');
+	const [age, setAge] = useState('...');
+
 	return (
+
 		<div>
+
+			<label> Nome: </label>
+			<input type="text" onChange={(event) => {setName(event.target.value)}}></input>
+
+			<label> Sobrenome: </label>
+			<input type="text" onChange={(event) => {setLastName(event.target.value)}}></input>
+
+			<label> Idade: </label>
+			<input type="number" onChange={(event) => {setAge(event.target.value)}}></input>
+
+			<hr />
+
+			<p>Seu nome é {name} {lastName} e você tem {age} anos.</p>
+
 		</div>
 	);
 }
