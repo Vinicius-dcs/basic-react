a383b3349aa109a054991d06c151c3abaf2eca20
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-09 14:48:04 -0300

    LESSON: Children and Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Header } from './components/Header';
+import { Photo } from './components/Photo';
+
 function App() {
 
 	return (
 		<div>
+			<Header title="Este é um exemplo" />
+			<Header title="Outro texto" />
+
+			Olá Mundo!
+
+			{/* Children ocorre quando tenho um componente criado por mim que não abre e fecha nele mesmo (<Photo />) e sim quando dentro dele existem outros elementos (<Photo> <img /> </Photo>) */}
+
+			<Photo legend="Google">
+				<img src="http://www.google.com.br/google.jpg"/> {/* Children */}
+			</Photo>
 		</div>
 	);
 }
