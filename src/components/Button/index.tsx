ae9a6f2210911501b80d444f729875ff0c4b8be3
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-09 17:16:17 -0300

    M3 L4: Style With CSS Modules

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,9 @@
-import './styles.css';
+import styles from './styles.module.css'; 
+
+/* Utilizando o CSS MOdules, o nome dos atributos CSS é modificado passando IDs na frente, ajuda a não criar conflitos de css, é mais vantajoso */
 
 export const Button = () => {
     return (
-        <button className='button'>Botão</button>
+        <button className={styles.button}>Botão</button>
     )
 }
