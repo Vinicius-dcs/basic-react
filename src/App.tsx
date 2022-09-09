322c6a711b64d3fccee63b4cd3ecc9ce64c8f6aa
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-09 16:11:08 -0300

    LESSON: Rendering additional content and toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+
 function App() {
+
+	const [show, setShow] = useState(false);
+	const [buttonText, setButtonText] = useState('Mostrar');
+
+	const toggleDiv = () => {
+		show === true ? setShow(false) : setShow(true);
+		toggleTextButton();
+	}
+
+	const toggleTextButton = () => {
+		buttonText === 'Mostrar' ? setButtonText('Esconder') : setButtonText('Mostrar');
+	}
 
 	return (
 		<div>
+			<button onClick={toggleDiv}>{buttonText}</button>
+
+			{show === true &&
+				<div>
+					Olha a DIV!
+				</div>
+			}
 		</div>
 	);
 }
