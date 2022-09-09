d798d96b42fffb7bd89df2a86e13bde0675e2b65
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-09 15:40:09 -0300

    LESSON: Project Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
 function App() {
+
+	const [number, setNumber] = useState(0)
+
+	const sumCount = () => {
+		setNumber(number + 1)
+	}
+
+	const decreaseCount = () => {
+		setNumber(number - 1)
+	}
 
 	return (
 		<div>
+			<button onClick={sumCount}>+</button>
+			<hr />
+			{number}
+			<hr />
+			<button onClick={decreaseCount}>-</button>
 		</div>
 	);
 }
