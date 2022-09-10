9cbb33ed9f0d11f94058aade5da3a87b483c2889
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-10 10:40:49 -0300

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { Container } from './components/Container'
-import { Button } from './components/Button'
-
 function App() {
 
 	return (
 		<div>
-			<Container color='black'>
-
-				<a href="" className="link">Link Qualquer</a>
-
-				<span>Texto do Componente</span>
-				<Button color='#FF0000'> Botão maior </Button>
-				<Button color='#00FF00' small> Botão menor </Button>
-			</Container>
+			...
 		</div>
 	);
 
