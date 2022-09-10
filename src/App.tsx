df00f4c1ca785d97f865791db63cb33c3e06f93c
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-10 11:27:31 -0300

    M3 L13: Responsivity in TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
 
 	return (
-		<div>
-			...
+		<div className="bg-black text-white p-4 text-2xl font-bold flex flex-col text-center md:flex-row md:text-green-500 lg:text-red-500 lg:justify-between">
+			Texto Qualquer
+			<button className="bg-green-300 text-black p-2 rounded-xl mt-3 md:ml-3 md:mt-0">Botão</button>
 		</div>
+
 	);
 
 }
