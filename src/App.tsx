deade5b08f955d73185307eb78115b195f0ea499
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-11 18:41:02 -0300

    M3 L14: Hover And States in TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
 
 	return (
-		<div className="bg-black text-white p-4 text-2xl font-bold flex flex-col text-center md:flex-row md:text-green-500 lg:text-red-500 lg:justify-between">
-			Texto Qualquer
-			<button className="bg-green-300 text-black p-2 rounded-xl mt-3 md:ml-3 md:mt-0">Botão</button>
+		<div className="bg-blue-300 p-5">
+			<input type="text" placeholder="Digite algo" className="border outline-none border-transparent bg-white p-3 rounded focus:ring-1 focus:ring-blue-700"/>
+			
+			<button className="ml-2 p-3 bg-blue-400 rounded text-white font-bold hover:bg-blue-500">Botão</button>
+			<button className="ml-2 p-3 bg-blue-400 rounded text-white font-bold disabled:opacity-50" disabled>Botão</button>
 		</div>
 
 	);
