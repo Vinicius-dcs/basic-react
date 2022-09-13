0e5e2d6edfd90a22a2690b571672272d3babe71c
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-12 21:19:05 -0300

    M3 L5: Request GET Simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Movie } from './types/Movie';
+import { Spinner } from '@chakra-ui/react';
 
 function App() {
-	const [name, setName] = useState('');
-	const [lastName, setLastName] = useState('');
-	const [fullName, setFullName]= useState('');
 
 	useEffect(() => {
-		setFullName(name + ' ' + lastName)
-	}, [name, lastName])
+		loadMovies();
+	}, [])
+
+	const [movies, setMovies] = useState<Movie[]>([]);
+	const [load, setLoad] = useState(true);
+
+	const loadMovies = () => {
+		fetch('https://api.b7web.com.br/cinema/')
+			.then((response) => {
+				return response.json();
+			})
+			.then((json) => {
+				setMovies(json);
+				setLoad(false);
+			})
+	}
 
 	return (
-		<div className="flex flex-col">
-			<input type="text" placeholder="Digite seu NOME" value={name} onChange={(event) => setName(event.target.value)}/>
-			<input type="text" placeholder="Digite seu SOBRENOME" value={lastName} onChange={(event) => setLastName(event.target.value)}/>
-			<p>Nome Completo: {fullName}</p>
+		<div>
+			{load &&
+				<div className="flex justify-center grid">
+					Carregando...
+					<div className="flex justify-center mt-5">
+						<Spinner
+							thickness='4px'
+							speed='0.65s'
+							emptyColor='gray.200'
+							color='blue.500'
+							size='xl'
+						/>
+					</div>
+				</div>
+			}
+			Total de Filmes: {movies.length}
+			<div className="grid grid-cols-6 gap-3">
+				{movies.map((item, index) => (
+					<div key={index}>
+						<img src={item.avatar} alt="" className="w-32 block"/>
+						{item.titulo}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 
