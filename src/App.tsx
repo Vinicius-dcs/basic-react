a068a4dc2650e3028a69b273bba74e866e899fe1
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-13 17:40:47 -0300

    M5 L18: Organizing Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import { useReducer } from 'react';
-
-type reducerState = {
-	count: number;
-}
-
-type reducerAction = {
-	type: string;
-}
-
-const initialState = { count: 0 };
-
-const reducer = (state: reducerState, action: reducerAction) => {
-	switch (action.type) {
-		case 'ADD':
-			return { ...state, count: state.count + 1 };
-			break;
-		case 'DEL':
-			if(state.count > - 0) {
-				return { ...state, count: state.count - 1 };
-			}
-			break;
-		case 'RESET':
-			return initialState;
-			break;
-	}
-
-	return state;
-}
+import { useContagem } from './hooks/reducers/contagem';
 
 function App() {
-	const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, dispatch] = useContagem();
 
 	return (
 		<div className="p-5">
