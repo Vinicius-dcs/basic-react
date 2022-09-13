be788a03a87c688fbe8a5c76ab34a3490ea55807
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-13 18:36:23 -0300

    M5 L21: Real Case using Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
-import { useContagem } from './hooks/reducers/contagem';
+import { usePeopleList } from './hooks/reducers/peopleList';
+import { useState } from 'react';
 
 function App() {
-	const [state, dispatch] = useContagem();
+	const [list, dispatch] = usePeopleList();
+	const [person, setPerson] = useState('');
+
+	const handleAddButton = () => {
+		if (person) {
+			dispatch({
+				type: 'ADD',
+				payload: {
+					name: person
+				}
+			});
+		}
+
+		setPerson('');
+	}
+
+	const deletePerson = (id: string) => {
+		dispatch({
+			type: 'DEL',
+			payload: { id }
+		});
+	}
+
+	const handleOrderButton = () => {
+		dispatch({
+			type: 'ORDER'
+		})
+	}
 
 	return (
 		<div className="p-5">
-			Contagem: {state.count}
+
+			<input type="text" value={person} onChange={(event) => { setPerson(event.target.value) }} className="border-2" />
+			<button className="p-1 bg-blue-500 m-1 rounded" onClick={handleAddButton}>Adicionar</button>
+			<button className="p-1 bg-blue-500 m-1 rounded" onClick={handleOrderButton}>Ordenar</button>
 
 			<hr />
 
-			<button className="p-2 bg-blue-400 m-3 rounded" onClick={() => dispatch({type: 'ADD'})}>Adicionar</button>
-			<button className="p-2 bg-blue-400 m-3 rounded" onClick={() => dispatch({type: 'DEL'})}>Remover</button>
-			<button className="p-2 bg-blue-400 m-3 rounded" onClick={() => dispatch({type: 'RESET'})}>Resetar</button>
-
+			Lista de Pessoas:
+			<ul>
+				{list.map((item, index) => (
+					<li
+						key={index}>{item.name}
+						<button onClick={() => deletePerson(item.id)}> [Deletar] </button>
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
