b84b3725bd2f18ff848f9f877cd7dcb08b365f9e
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-26 22:38:33 -0300

    M7 L11: Routes in object

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useRoutes } from 'react-router-dom';
 import { About } from '../pages/About';
 import { AboutItem } from '../pages/AboutItem';
 import { Home } from '../pages/Home';
@@ -6,15 +6,22 @@ import { NotFound } from '../pages/NotFound';
 import { RequireAuth } from '../RequireAuth';
 
 export const MainRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sobre" element={
-                <RequireAuth>
-                    <About />
-                </RequireAuth>} />
-            <Route path="/sobre/:slug" element={<AboutItem />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    );
+    return useRoutes([
+        { path: '/', element: <Home /> },
+        { path: '/sobre', element: <RequireAuth><About /></RequireAuth> },
+        { path: '/sobre/:slug', element: <AboutItem /> },
+        { path: '*', element: <NotFound /> },
+    ]);
+
+    /*  return (
+         <Routes>
+             <Route path="/" element={<Home />} />
+             <Route path="/sobre" element={
+                 <RequireAuth>
+                     <About />
+                 </RequireAuth>} />
+             <Route path="/sobre/:slug" element={<AboutItem />} />
+             <Route path="*" element={<NotFound />} />
+         </Routes>
+     ); */
 }
