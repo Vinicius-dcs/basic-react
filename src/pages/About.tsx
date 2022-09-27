a6c48e51308a9d3ff42ed59976232aa0423579c6
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-26 21:06:03 -0300

    M7 L5: Links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export const About = () => {
     return (
         <div>
             Página Sobre
             <ul>
-                <li>Vinicius</li>
-                <li>Costa</li>
+                <li><Link to="/sobre/vinicius">Vinicius</Link></li>
+                <li><Link to="/sobre/costa">Costa</Link></li>
             </ul>
         </div>
     );
