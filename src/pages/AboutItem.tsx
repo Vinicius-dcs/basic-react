858ee5455552111bf5ab503d0985423883f3ffb1
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-26 21:27:22 -0300

    M7 L7: Redirection of routes

--- src/pages/AboutItem.tsx
+++ src/pages/AboutItem.tsx
@@ -1,11 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export const AboutItem = () => {
     const params = useParams();
+    const navigate = useNavigate();
+
+    const handleBackButton = () => {
+        navigate(-1);
+    }
+
+    const handleHomeButton = () => {
+        navigate('/');
+    }
 
     return (
         <div>
             Página Sobre {params.slug}
+            <hr />
+            <button onClick={handleBackButton}>Voltar</button>
+            <br />
+            <button onClick={handleHomeButton}>Home</button>
         </div>
     );
 }
