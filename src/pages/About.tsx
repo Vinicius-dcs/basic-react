a1ba94a5e6d719d1fd6af39373a3bbd83e657718
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-09-26 21:07:27 -0300

    M7 L5: Links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,6 +7,8 @@ export const About = () => {
             <ul>
                 <li><Link to="/sobre/vinicius">Vinicius</Link></li>
                 <li><Link to="/sobre/costa">Costa</Link></li>
+
+                {/* A vantagem de utilizar o link do react é que o componente Link não recarrega a página toda e sim somente o que deve ser renderizado, diferente do <a href> do html, que recarrega a página inteira. */}
             </ul>
         </div>
     );
